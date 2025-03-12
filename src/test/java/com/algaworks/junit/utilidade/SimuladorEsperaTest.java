0a7f720b1d42ac7a0e96fc5182c223a279381fc7
Gilson Silva <gilsonsilvati@gmail.com>
2025-03-12 16:18:21 -0300

    test: 2.10. Ignorando execução dos testes condicionalmente com Assumptions

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import java.time.Duration;
 
@@ -9,7 +10,12 @@ import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 class SimuladorEsperaTest {
 
     @Test
+//    @Disabled("Não é mais aplicavél")
+    @EnabledIfEnvironmentVariable(named = "ENV", matches = "DEV")
     void deveEsperarENaoDarTimeout() {
+
+//        Assumptions.assumeTrue("PROD".equals(System.getenv("ENV")),
+//                () -> "Abortando teste: Não deve ser executado em PROD");
 
 //        assertTimeout(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofSeconds(10)));
 //        assertTimeoutPreemptively(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofSeconds(10)));
