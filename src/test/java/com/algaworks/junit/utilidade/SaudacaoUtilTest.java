82f6fd0ee524b761036b6586690c1abdf8a59066
Gilson Silva <gilsonsilvati@gmail.com>
2025-03-13 13:55:06 -0300

    test: 2.12. Desafio - Escrevendo testes faltantes do SaudacaoUtil

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -2,6 +2,7 @@ package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Test;
 
+import static com.algaworks.junit.utilidade.SaudacaoUtil.saudar;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -9,17 +10,33 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class SaudacaoUtilTest {
 
     @Test
-    void deverSaudarSucesso() {
+    void deverSaudarBomDia() {
 
-        var saudacao = SaudacaoUtil.saudar(9);
+        var saudacao = saudar(9);
 
         assertEquals("Bom dia", saudacao, "Saudação incorreta!");
     }
 
     @Test
+    void deverSaudarBoaTarde() {
+
+        var saudacao = saudar(15);
+
+        assertEquals("Boa tarde", saudacao, "Saudação incorreta!");
+    }
+
+    @Test
+    void deverSaudarBoaNoite() {
+
+        var saudacao = saudar(20);
+
+        assertEquals("Boa noite", saudacao, "Saudação incorreta!");
+    }
+
+    @Test
     void deveLancarException() {
 
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> SaudacaoUtil.saudar(-10));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> saudar(-10));
 
         assertEquals("Hora inválida", exception.getMessage());
     }
@@ -27,6 +44,6 @@ class SaudacaoUtilTest {
     @Test
     void naoDeveLancarException() {
 
-        assertDoesNotThrow(() -> SaudacaoUtil.saudar(0));
+        assertDoesNotThrow(() -> saudar(0));
     }
 }
