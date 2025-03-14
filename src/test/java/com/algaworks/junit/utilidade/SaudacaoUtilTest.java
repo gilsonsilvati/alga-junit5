4a54c237668bfc990c237467abec4c4b5d8ea96a
Gilson Silva <gilsonsilvati@gmail.com>
2025-03-14 10:46:54 -0300

    test: 3.1. Organizando testes com o padrão Triple A

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static com.algaworks.junit.utilidade.SaudacaoUtil.saudar;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -9,57 +10,78 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SaudacaoUtilTest {
 
+    /*
+     * Organizando testes com o padrão Triple A
+     *
+     * Arrange: Arranjo (Cenário)
+     * Act: Agir (Executar)
+     * Assert: Afirmar (Asserção)
+     */
+
     @Test
     void deverSaudarBomDia() {
+        // Arrange
+        var horaValida = 9;
 
-        var saudacao = saudar(9);
+        // Act
+        var saudacao = saudar(horaValida);
 
+        // Assert
         assertEquals("Bom dia", saudacao, "Saudação incorreta!");
     }
 
     @Test
     void deverSaudarBomDiaApartir5h() {
+        var horaValida = 5;
 
-        var saudacao = saudar(5);
+        var saudacao = saudar(horaValida);
 
         assertEquals("Bom dia", saudacao, "Saudação incorreta!");
     }
 
     @Test
     void deverSaudarBoaTarde() {
+        var horaValida = 15;
 
-        var saudacao = saudar(15);
+        var saudacao = saudar(horaValida);
 
         assertEquals("Boa tarde", saudacao, "Saudação incorreta!");
     }
 
     @Test
     void deverSaudarBoaNoite() {
+        var horaValida = 20;
 
-        var saudacao = saudar(20);
+        var saudacao = saudar(horaValida);
 
         assertEquals("Boa noite", saudacao, "Saudação incorreta!");
     }
 
     @Test
     void deverSaudarBoaNoiteAte4h() {
+        var horaValida = 4;
 
-        var saudacao = saudar(4);
+        var saudacao = saudar(horaValida);
 
         assertEquals("Boa noite", saudacao, "Saudação incorreta!");
     }
 
     @Test
     void deveLancarException() {
+        var horaInvalida = -10;
 
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> saudar(-10));
+        Executable chamadaInvalida = () -> saudar(horaInvalida);
 
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, chamadaInvalida);
         assertEquals("Hora inválida", exception.getMessage());
     }
 
     @Test
     void naoDeveLancarException() {
+        var horaValida = 0;
 
-        assertDoesNotThrow(() -> saudar(0));
+        Executable chamadaValida = () -> saudar(horaValida);
+
+        assertDoesNotThrow(chamadaValida);
     }
 }
