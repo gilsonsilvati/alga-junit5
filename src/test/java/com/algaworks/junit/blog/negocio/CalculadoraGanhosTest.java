01da533aa8035b860c19dfb6f70c5a4a1bfb6de9
Gilson Silva <gilsonsilvati@gmail.com>
2025-03-18 16:24:33 -0300

    test: 3.7. Um teste deve ter uma única asserção?

--- src/test/java/com/algaworks/junit/blog/negocio/CalculadoraGanhosTest.java
+++ src/test/java/com/algaworks/junit/blog/negocio/CalculadoraGanhosTest.java
@@ -6,72 +6,65 @@ import com.algaworks.junit.blog.modelo.Post;
 import com.algaworks.junit.blog.utilidade.ProcessadorTextoSimples;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class CalculadoraGanhosTest {
-
-    static final int VALOR_PAGO_PALAVRA = 5;
 
     static CalculadoraGanhos calculadora;
 
     Editor autor;
     Post post;
 
-    // Arrange
     @BeforeAll
     static void beforeAll() {
-//        System.out.println("Antes de TODOS os testes");
-
         calculadora = new CalculadoraGanhos(new ProcessadorTextoSimples(), BigDecimal.TEN);
     }
 
-    // Arrange
     @BeforeEach
-    void setUp() {
-//        System.out.println("Antes de CADA teste");
-
-        autor = new Editor(1L, "Gilson", "gilson@gmail.com", new BigDecimal(VALOR_PAGO_PALAVRA), true);
+    void beforeEach() {
+        autor = new Editor(1L, "Alex", "alex@gmail.com", new BigDecimal(5), true);
 
         post = new Post(1L, "Ecossistema Java", "O ecossistema do Java é muito maduro", autor,
                 "ecossistema-java-abc123", null, false, false);
     }
 
-//    @AfterAll
-//    static void afterAll() {
-//        System.out.println("Depois de TODOS os testes");
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//        System.out.println("Depois de CADA teste");
-//    }
-
     @Test
-    void deveCalcularGanhosComPremium() {
+    void Dado_um_post_e_autor_premium_Quando_calcular_ganhos_Entao_deve_retornar_valor_com_bonus() {
 
-        // Act
         Ganhos ganhos = calculadora.calcular(post);
 
-        // Assert
-        assertEquals(new BigDecimal(45), ganhos.getTotalGanho());
+        assertEquals(new BigDecimal("45"), ganhos.getTotalGanho());
+    }
+
+    @Test
+    void Dado_um_post_e_autor_Quando_calcular_ganhos_Entao_deve_retornar_quantidade_de_palavras_no_post() {
+
+        Ganhos ganhos = calculadora.calcular(post);
+
         assertEquals(7, ganhos.getQuantidadePalavras());
+    }
+
+    @Test
+    void Dado_um_post_e_autor_Quando_calcular_ganhos_Entao_deve_retornar_valor_pago_por_palavra_do_autor() {
+
+        Ganhos ganhos = calculadora.calcular(post);
+
         assertEquals(autor.getValorPagoPorPalavra(), ganhos.getValorPagoPorPalavra());
     }
 
     @Test
-    void deveCalcularGanhosSemPremium() {
+    void Dado_um_post_e_autor_nao_premium_Quando_calcular_ganhos_Entao_deve_retornar_valor_sem_bonus() {
         autor.setPremium(false);
 
-        // Act
         Ganhos ganhos = calculadora.calcular(post);
 
-        // Assert
-        assertEquals(new BigDecimal(35), ganhos.getTotalGanho());
-        assertEquals(7, ganhos.getQuantidadePalavras());
-        assertEquals(autor.getValorPagoPorPalavra(), ganhos.getValorPagoPorPalavra());
+        assertEquals(new BigDecimal("35"), ganhos.getTotalGanho());
     }
 }
