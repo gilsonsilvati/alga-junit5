2906047d4b4a0759359f1ccd21332cf9cea55f0f
Gilson Silva <gilsonsilvati@gmail.com>
2025-03-21 14:20:34 -0300

    test: 3.9. Desafio - Implemente a lógica e testes de um carrinho de compras

--- src/main/java/com/algaworks/junit/ecommerce/CarrinhoCompra.java
+++ src/main/java/com/algaworks/junit/ecommerce/CarrinhoCompra.java
@@ -8,78 +8,101 @@ import java.util.Optional;
 
 public class CarrinhoCompra {
 
-	private final Cliente cliente;
-	private final List<ItemCarrinhoCompra> itens;
+    private final Cliente cliente;
+    private final List<ItemCarrinhoCompra> itens;
 
-	public CarrinhoCompra(Cliente cliente) {
-		this(cliente, new ArrayList<>());
-	}
-
-	public CarrinhoCompra(Cliente cliente, List<ItemCarrinhoCompra> itens) {
-		Objects.requireNonNull(cliente);
-		Objects.requireNonNull(itens);
-		this.cliente = cliente;
-		this.itens = new ArrayList<>(itens); //Cria lista caso passem uma imutável
-	}
-
-	public List<ItemCarrinhoCompra> getItens() {
-		//TODO deve retornar uma nova lista para que a antiga não seja alterada
-		return null;
-	}
-
-	public Cliente getCliente() {
-		return cliente;
-	}
-
-	public void adicionarProduto(Produto produto, int quantidade) {
-		//TODO parâmetros não podem ser nulos, deve retornar uma exception
-		//TODO quantidade não pode ser menor que 1
-		//TODO deve incrementar a quantidade caso o produto já exista
-	}
-
-	public void removerProduto(Produto produto) {
-		//TODO parâmetro não pode ser nulo, deve retornar uma exception
-		//TODO caso o produto não exista, deve retornar uma exception
-		//TODO deve remover o produto independente da quantidade
-	}
-
-	public void aumentarQuantidadeProduto(Produto produto) {
-		//TODO parâmetro não pode ser nulo, deve retornar uma exception
-		//TODO caso o produto não exista, deve retornar uma exception
-		//TODO deve aumentar em um quantidade do produto
-	}
-
-    public void diminuirQuantidadeProduto(Produto produto) {
-		//TODO parâmetro não pode ser nulo, deve retornar uma exception
-		//TODO caso o produto não exista, deve retornar uma exception
-		//TODO deve diminuir em um quantidade do produto, caso tenha apenas um produto, deve remover da lista
-	}
-
-    public BigDecimal getValorTotal() {
-		//TODO implementar soma do valor total de todos itens
-		return null;
+    public CarrinhoCompra(Cliente cliente) {
+        this(cliente, new ArrayList<>());
     }
 
-	public int getQuantidadeTotalDeProdutos() {
-		//TODO retorna quantidade total de itens no carrinho
-		//TODO Exemplo em um carrinho com 2 itens, com a quantidade 2 e 3 para cada item respectivamente, deve retornar 5
-		return 0;
-	}
+    public CarrinhoCompra(Cliente cliente, List<ItemCarrinhoCompra> itens) {
+        Objects.requireNonNull(cliente);
+        Objects.requireNonNull(itens);
+        this.cliente = cliente;
+        this.itens = new ArrayList<>(itens); //Cria lista caso passem uma imutável
+    }
 
-	public void esvaziar() {
-		//TODO deve remover todos os itens
-	}
+    public List<ItemCarrinhoCompra> getItens() {
+        return new ArrayList<>(itens);
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		CarrinhoCompra that = (CarrinhoCompra) o;
-		return Objects.equals(itens, that.itens) && Objects.equals(cliente, that.cliente);
-	}
+    public Cliente getCliente() {
+        return cliente;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(itens, cliente);
-	}
+    public void adicionarProduto(Produto produto, int quantidade) {
+        Objects.requireNonNull(produto);
+        validaQuantidade(quantidade);
+
+        encontrarItemPeloProduto(produto)
+                .ifPresentOrElse(item -> item.adicionarQuantidade(quantidade), () -> adicionarNovoItem(produto, quantidade));
+    }
+
+    public void removerProduto(Produto produto) {
+        Objects.requireNonNull(produto);
+        ItemCarrinhoCompra item = encontrarItemPeloProduto(produto).orElseThrow(RuntimeException::new);
+        this.itens.remove(item);
+    }
+
+    public void aumentarQuantidadeProduto(Produto produto) {
+        Objects.requireNonNull(produto);
+        ItemCarrinhoCompra item = encontrarItemPeloProduto(produto).orElseThrow(RuntimeException::new);
+        item.adicionarQuantidade(1);
+    }
+
+    public void diminuirQuantidadeProduto(Produto produto) {
+        Objects.requireNonNull(produto);
+        ItemCarrinhoCompra item = encontrarItemPeloProduto(produto).orElseThrow(RuntimeException::new);
+        if (item.getQuantidade() == 1) {
+            itens.remove(item);
+        } else {
+            item.subtrairQuantidade(1);
+        }
+    }
+
+    public BigDecimal getValorTotal() {
+        return this.itens.stream()
+                .map(ItemCarrinhoCompra::getValorTotal)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public int getQuantidadeTotalDeProdutos() {
+        return this.itens.stream()
+                .map(ItemCarrinhoCompra::getQuantidade)
+                .reduce(0, Integer::sum);
+    }
+
+    public void esvaziar() {
+        this.itens.clear();
+    }
+
+    private void validaQuantidade(int quantidade) {
+        if (quantidade <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void adicionarNovoItem(Produto produto, int quantidade) {
+        this.itens.add(new ItemCarrinhoCompra(produto, quantidade));
+    }
+
+    private Optional<ItemCarrinhoCompra> encontrarItemPeloProduto(Produto produto) {
+        return this.itens
+                .stream()
+                .filter(item -> item.getProduto().equals(produto))
+                .findFirst();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CarrinhoCompra that = (CarrinhoCompra) o;
+        return Objects.equals(itens, that.itens) && Objects.equals(cliente, that.cliente);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(itens, cliente);
+    }
 }
